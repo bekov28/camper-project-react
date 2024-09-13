506c6047fc3920898262be60827f7064fe43218b
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-09-14 01:14:37 +0900

    push

--- src/components/mail/index.jsx
+++ src/components/mail/index.jsx
@@ -2,8 +2,15 @@ import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "../tabs/style";
 import { SendBtn, SendContainer } from "../tabs/style";
+import { Alert } from "@mui/material";
+import Button from "@mui/joy/Button";
+import Snackbar from "@mui/joy/Snackbar";
+import PlaylistAddCheckCircleRoundedIcon from "@mui/icons-material/PlaylistAddCheckCircleRounded";
 
 export const EmailComponent = () => {
+  const [open, setOpen] = React.useState(false);
+  const [errorOpen, setErrorOpen] = React.useState(false);
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -16,19 +23,68 @@ export const EmailComponent = () => {
       .then(
         () => {
           console.log("SUCCESS!");
+          setOpen(true);
         },
         (error) => {
           console.log("FAILED...", error.text);
+          setErrorOpen(true);
         }
       );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <input type="text" name="user_name" placeholder="Your name" />
-      <input type="email" name="user_email" placeholder="Your email" />
-      <textarea name="message" placeholder="Your question" />
-      <input type="submit" value="Send question" className="SendBtn" />
-    </form>
+    <>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" placeholder="Your name" required />
+        <input
+          type="email"
+          name="user_email"
+          placeholder="Your email"
+          required
+        />
+        <textarea name="message" placeholder="Your question" required />
+        <input type="submit" value="Send question" className="SendBtn" />
+      </form>
+      <Snackbar
+        variant="soft"
+        color="success"
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        startDecorator={<PlaylistAddCheckCircleRoundedIcon />}
+        endDecorator={
+          <Button
+            onClick={() => setOpen(false)}
+            size="sm"
+            variant="soft"
+            color="success"
+          >
+            Dismiss
+          </Button>
+        }
+      >
+        Your message was sent successfully.
+      </Snackbar>
+      <Snackbar
+        variant="solid"
+        color="danger"
+        open={errorOpen}
+        onClose={() => setErrorOpen(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        startDecorator={<PlaylistAddCheckCircleRoundedIcon />}
+        endDecorator={
+          <Button
+            onClick={() => setErrorOpen(false)}
+            size="sm"
+            variant="solid"
+            color="danger"
+          >
+            Dismiss
+          </Button>
+        }
+      >
+        Your message wasnt sent. Please check again!
+      </Snackbar>
+    </>
   );
 };
